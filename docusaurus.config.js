928e627011512e12c8cb8a82a37e7935dbc7c5dc
tienvo <tien.vo@sunbytes.vn>
2023-02-08 17:53:06 +0700

    Update docusaurus

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,34 +1,42 @@
-module.exports = {
+// @ts-check
+// Note: type annotations allow type checking and IDEs autocompletion
+
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').Config} */
+const config = {
   title: "Tien Vo's Blog",
   tagline: 'Symfony 4, Software Development, Software Architect, Software Testing',
-  url: 'https://tienvx.github.io',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
+
+  // Set the production url of your site here
+  url: 'https://tienvx.github.io',
+  // Set the /<baseUrl>/ pathname under which your site is served
+  // For GitHub pages deployment, it is often '/<projectName>/'
+  baseUrl: '/',
+
+  // GitHub pages deployment config.
+  // If you aren't using GitHub pages, you don't need these.
   organizationName: 'tienvx', // Usually your GitHub org/user name.
   projectName: 'tienvx.github.io', // Usually your repo name.
-  themeConfig: {
-    navbar: {
-      title: "Tien Vo's Blog",
-      logo: {
-        alt: 'Blog Logo',
-        src: 'img/logo.svg',
-      },
-      items: [
-        {to: 'projects', label: 'Projects', position: 'left'},
-        {to: 'cv/redirect', label: 'CV', position: 'left', target: '_blank', rel: 'nofollow'},
-      ],
-    },
-    footer: {
-      style: 'dark',
-      copyright: `Copyright © ${new Date().getFullYear()} Tien Vo, Inc. Built with Docusaurus.`,
-    },
+
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+
+  // Even if you don't use internalization, you can use this field to set useful
+  // metadata like html lang. For example, if your site is Chinese, you may want
+  // to replace "en" with "zh-Hans".
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en'],
   },
+
   presets: [
     [
-      '@docusaurus/preset-classic',
-      {
+      'classic',
+      /** @type {import('@docusaurus/preset-classic').Options} */
+      ({
         docs: false,
         blog: {
           path: './blog',
@@ -37,7 +45,35 @@ module.exports = {
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
         },
-      },
+      }),
     ],
   ],
+
+  themeConfig:
+    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+    ({
+      // Replace with your project's social card
+      image: 'img/docusaurus-social-card.jpg',
+      navbar: {
+        title: "Tien Vo's Blog",
+        logo: {
+          alt: 'Blog Logo',
+          src: 'img/logo.svg',
+        },
+        items: [
+          {to: 'projects', label: 'Projects', position: 'left'},
+          {to: 'cv/redirect', label: 'CV', position: 'left', target: '_blank', rel: 'nofollow'},
+        ],
+      },
+      footer: {
+        style: 'dark',
+        copyright: `Copyright © ${new Date().getFullYear()} Tien Vo. Built with Docusaurus.`,
+      },
+      prism: {
+        theme: lightCodeTheme,
+        darkTheme: darkCodeTheme,
+      },
+    }),
 };
+
+module.exports = config;
